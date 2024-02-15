a31b7d9c798f2808caa4a84fbdfa222d95bc85a0
manjusomu <manjusomu2001@gmail.com>
2024-02-15 14:44:03 +0530

    changes added

---
+++ OddEven.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTask
+{
+    public class OddEven
+    {
+
+
+        public void CheckOddEven()
+        {
+            int i=5;
+        
+         
+            if (i % 2 == 0)
+            {
+                Console.WriteLine("Even number");
+            }
+            else
+            {
+                Console.WriteLine("not even number");
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using AppTask;
+
+OddEven obj=new OddEven();
+obj.CheckOddEven();
+
